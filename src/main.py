c4ed557d4ab6569dd1c70298ed5ec8bf979536b6
Ilhan Ozgen <iozgen@lbl.gov>
2020-05-14 16:10:18 -0700

    add meshing function

--- src/main.py
+++ src/main.py
@@ -1,16 +1,37 @@
 from mapper_class import MAPPER
+import matplotlib.pyplot as plt
+import numpy as np
+import pyvista as pv
 
 mapper = MAPPER("dem.asc", "../input")
+
+# --- A: DBG ---
+# we need to set the lower left corner to 0
+# this must be fixed once the issue in tinerator is solved
+mapper.dem.xll_corner = 0.0
+mapper.dem.yll_corner = 0.0
+# --- E: DBG ---
+
 mapper.add_horizon("bottom_sh.asc")
 mapper.add_horizon("horizon_21.asc")
 
-print(mapper.horizon_dims)
-print(mapper.horizons)
+#for i in range(len(mapper.horizons)):
+#    _h = np.ma.masked_less(mapper.horizons[i], 0.0)
+#    plt.imshow(_h)
+#    plt.show()
 
-import matplotlib.pyplot as plt
-import numpy as np
-for i in range(len(mapper.horizons)):
-    _h = np.ma.masked_less(mapper.horizons[i], 0.0)
-    _n = mapper.horizon_dims[i]
-    plt.imshow(np.reshape(_h, (_n[0], _n[1])))
+names = [ - 0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5 ]
+vals = mapper.map_horizons_to_layers()
+
+mapper.do_mesh(80.0)
+
+counter = 1
+for v in vals:
+    print("[OK] %d" %counter)
+    plt.imshow(np.ma.masked_less(v, -9000), interpolation = 'none', cmap = 'Spectral_r')
+    plt.clim(0, 7)
+    cb = plt.colorbar(ticks = range(8), boundaries = names)
     plt.show()
+    counter = counter + 1
+
+pv.read("_mesh.exo").plot()
--- src/mapper_class.py
+++ src/mapper_class.py
@@ -18,35 +18,102 @@ class MAPPER():
     dirpath (str)  : Directory path for input files
     '''
 
-    def __init__(self, filepath:str, dirpath:str = "input"):
+    def __init__(self, filepath:str, dirpath:str = "input", layerpath:str = "layer-thickness.dat"):
 
         self.ok = "[OK]"
         self.na = "[!!]"
 
         self.dirpath = dirpath
         self.dem = tin.load.from_file(dirpath + "/" + filepath)
+
+        self.layers = np.genfromtxt(dirpath + "/" + layerpath, comments = '#', delimiter = ',')
         
         self.horizons = []
-        self.horizon_dims = []
 
-        # --- A: DBG ---
-        # we need to set the lower left corner to 0
-        # this must be fixed once the issue in tinerator is solved
-        self.dem.xll_corner = 0.0
-        self.dem.yll_corner = 0.0
-        # --- E: DBG ---
+    def do_mesh(self, dx_min:float, fname:str="_mesh.exo", dx_max:float = 0.0, eps:float = 1.0e-2):
+        '''
+        generate the mesh that we will map the values onto
+        '''
+        if dx_max < dx_min:
+            self.dem.build_uniform_triplane(dx_min)
+        else:
+            self.dem.watershed_delineation(eps)
+            self.dem.build_refined_triplane(dx_min, dx_max)
+        self.dem.build_layered_mesh(self.layers[:,1], matids = self.layers[:,0].astype(int))
+        tin.dump.to_exodus(self.dem, '_mesh.exo')
 
+            
     def add_horizon(self, filepath:str, sep:str = " ", comment_char:str = "#"):
+        '''
+        read in the upper boundary of a geological layer and add it
+        to a list that will be traversed during the mapping process
 
+        # Attributes
+        filepath     : file containing the horizon
+        sep          : delimiter in the file
+        comment_char : comment indicator in the file
+        '''
         _horizon = np.genfromtxt(self.dirpath + "/" + filepath,
                                  delimiter = sep, comments = comment_char)
-        _ny, _nx = _horizon.shape
-        self.horizon_dims.append([_ny, _nx])
-        self.horizons.append(_horizon.flatten())
+        self.horizons.append(_horizon)
 
         print(self.ok + " added " + filepath + " to the horizons.")
 
+        return self.horizons
+
+    def map_horizons_to_layers(self):
+        '''
+        traverse the list of horizons and create an array that holds
+        the geological layer id for each point in the dem
+        '''
+
+        mapped_vals = []
         
+        n_horizons = len(self.horizons)
 
+        if n_horizons == 0:
+            print(self.na + " no layers defined. cannot proceed.")
+            import sys
+            sys.exit(-1)
 
+        layer_ids = self.layers[:,0]
+        layer_dzs = self.layers[:,1]
 
+        n_layers = len(layer_ids)
+
+        ny, nx = self.dem.dem.shape
+
+        _top = np.copy(self.dem.dem)
+        
+        for i in range(n_layers):
+            _up  = np.zeros((ny, nx), dtype=int) - 9999
+            _do  = np.zeros((ny, nx), dtype=int) - 9999
+            _surf = _top - 0.5 * layer_dzs[i]
+            _soil_ids = np.zeros((ny, nx))
+
+            for row in range(ny):
+                for col in range(nx):
+                    _z = _surf[row, col]
+                    _ab = 10000.0
+                    _be = 10000.0
+                    if _z > self.horizons[0][row, col]:
+                        _up[row, col] = 0
+                    elif _z > 0.0:
+                        for horizon_index in range(len(self.horizons)):
+                            _h = self.horizons[horizon_index]
+                            _delta = _h[row, col] - _z
+                            if _delta >= 0.0 and _delta < _ab:
+                                _ab = _delta
+                                _up[row, col] = horizon_index
+                            elif _delta < 0.0 and np.abs(_delta) < _be:
+                                _be = np.abs(_delta)
+                                _do[row, col] = horizon_index
+
+            _up[np.where(_up < -1000)] = _do[np.where(_up < -1000)]
+            print(self.ok + " geology type mapped to %f m." %np.mean(np.ma.masked_less(_top, 0.0)))
+            _top = _surf - 0.5 * layer_dzs[i]
+            mapped_vals.append(_up)
+
+            np.savetxt("soil-id-layer-" + str(i) + ".asc", _up, delimiter = " ")
+
+        return mapped_vals
